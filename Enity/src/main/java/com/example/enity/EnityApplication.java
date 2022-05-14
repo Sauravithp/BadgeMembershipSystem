bebb2b3096517023175fb9731b180a0bdcdb13af
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-14 15:22:39 -0500

    adding module for entity

---
+++ Enity/src/main/java/com/example/enity/EnityApplication.java
@@ -0,0 +1,13 @@
+package com.example.enity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnityApplication.class, args);
+    }
+
+}
---
+++ Enity/src/test/java/com/example/enity/EnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
