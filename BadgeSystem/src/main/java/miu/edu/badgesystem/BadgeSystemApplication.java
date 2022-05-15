24fbbf3426c6649c724f0b3ea9f0f3f7dd9cdef5
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-15 08:46:10 -0500

    complete

---
+++ BadgeSystem/src/main/java/miu/edu/badgesystem/BadgeSystemApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.badgesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeSystemApplication.class, args);
+    }
+
+}
