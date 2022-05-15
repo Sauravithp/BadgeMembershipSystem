2bc64afe00d54b304960572f6c0431808f82a814
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-15 14:45:21 -0500

    add login

--- BadgeSystem/src/main/java/miu/edu/badgesystem/controller/LoginController.java
+++ BadgeSystem/src/main/java/miu/edu/badgesystem/controller/LoginController.java
@@ -2,15 +2,18 @@ package miu.edu.badgesystem.controller;
 
 import miu.edu.badgesystem.dto.request.LoginRequestDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Login")
+@RequestMapping("/login")
 public class LoginController {
 
     @PostMapping
-    public void login(LoginRequestDTO loginRequestDTO){
+    public void login(@RequestBody LoginRequestDTO loginRequestDTO){
 
+        System.out.println(loginRequestDTO.getUsername());
+        System.out.println(loginRequestDTO.getPassword());
     }
 }
