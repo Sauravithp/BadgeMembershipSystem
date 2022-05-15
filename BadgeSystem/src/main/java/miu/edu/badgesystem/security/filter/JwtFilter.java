25c2ce9e4328d7515714dab5b1b4ad4f7ab43470
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-15 16:01:00 -0500

    complete filter

--- BadgeSystem/src/main/java/miu/edu/badgesystem/security/filter/JwtFilter.java
+++ BadgeSystem/src/main/java/miu/edu/badgesystem/security/filter/JwtFilter.java
@@ -1,6 +1,7 @@
 package miu.edu.badgesystem.security.filter;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.net.http.HttpRequest;
 
 @Component
 public class JwtFilter implements Filter {
@@ -15,8 +17,19 @@ public class JwtFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Test");
-        chain.doFilter(request,response);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String url = String.valueOf(httpRequest.getRequestURL());
+        if (url.contains("/login")) {
+            chain.doFilter(request, response);
+        } else {
+            String header=httpRequest.getHeader("Authorization");
+            if(ObjectUtils.isEmpty(header)){
+                throw new NullPointerException("Error");
+            }else {
+                chain.doFilter(request,response);
+            }
+        }
+
     }
 
 }
