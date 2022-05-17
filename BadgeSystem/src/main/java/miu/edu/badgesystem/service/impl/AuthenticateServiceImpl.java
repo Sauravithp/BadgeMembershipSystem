e0d9cfcad66b708363fa00b7364237ef5bcc6bad
Abanoub <Abanob.George@vodafone.com>
2022-05-17 11:33:57 -0500

    Authenticate User before generating JWT

--- BadgeSystem/src/main/java/miu/edu/badgesystem/service/impl/AuthenticateServiceImpl.java
+++ BadgeSystem/src/main/java/miu/edu/badgesystem/service/impl/AuthenticateServiceImpl.java
@@ -1,10 +1,13 @@
 package miu.edu.badgesystem.service.impl;
 
+import miu.edu.badgesystem.Validator.LoginValidator;
 import miu.edu.badgesystem.config.KeycloakProvider;
 import miu.edu.badgesystem.dto.request.LoginRequestDTO;
+import miu.edu.badgesystem.repository.UserRepository;
 import miu.edu.badgesystem.service.AuthenticateService;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.AccessTokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.BadRequestException;
@@ -12,6 +15,8 @@ import javax.ws.rs.BadRequestException;
 @Service
 public class AuthenticateServiceImpl implements AuthenticateService {
 
+    @Autowired
+    UserRepository userRepository;
     private final KeycloakProvider kcProvider;
 
     public AuthenticateServiceImpl(KeycloakProvider kcProvider) {
@@ -20,23 +25,26 @@ public class AuthenticateServiceImpl implements AuthenticateService {
 
     @Override
     public String loginUser(LoginRequestDTO requestDTO) {
+        var user = userRepository.getUser(requestDTO.getUsername());
+        String token = null;
+        if(LoginValidator.checkPassword(requestDTO.getPassword(), user.getPassword())){
+            token = getJwtToken(requestDTO);
+        }
+        return token;
+    }
 
-//        Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(requestDTO.getUsername(), requestDTO.getPassword()));
-
-            Keycloak keycloak = kcProvider.newKeycloakBuilderWithPasswordCredentials(
-                    requestDTO.getUsername(),
-                    requestDTO.getPassword()
-            ).build();
-            AccessTokenResponse accessTokenResponse = null;
-            try {
-                accessTokenResponse = keycloak.tokenManager().getAccessToken();
-                return accessTokenResponse.getToken();
-            } catch (BadRequestException ex) {
-                System.out.println("invalid account. User probably hasn't verified email.");
-                return null;
-            }
-
-
+    private String getJwtToken(LoginRequestDTO requestDTO) {
+        Keycloak keycloak = kcProvider.newKeycloakBuilderWithPasswordCredentials(
+                requestDTO.getUsername(),
+                requestDTO.getPassword()
+        ).build();
+        AccessTokenResponse accessTokenResponse = null;
+        try {
+            accessTokenResponse = keycloak.tokenManager().getAccessToken();
+            return accessTokenResponse.getToken();
+        } catch (BadRequestException ex) {
+            System.out.println("invalid account. User probably hasn't verified username.");
+            return null;
+        }
     }
 }
