a00c2bf112131d9d1b5bc00aa5e7d367fab35190
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-17 23:36:01 -0500

    complete

--- TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/config/WebSecurityConfiguration.java
+++ TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/config/WebSecurityConfiguration.java
@@ -1,32 +1,28 @@
 package miu.edu.transactionmanagementsystem.config;
 
+import miu.edu.transactionmanagementsystem.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
+    @Autowired
+    private JwtFilter jwtFilter;
 
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
             http.csrf().disable()
+                    .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
                     .authorizeHttpRequests()
-                    .antMatchers("/*").permitAll()
+                    .antMatchers("/").permitAll()
                     .antMatchers("/eureka/css/**","/eureka/images/**","/eureka/fonts/**", "/eureka/js/**").permitAll()
                     .anyRequest()
                     .authenticated()
--- TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/filter/JwtFilter.java
+++ TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/filter/JwtFilter.java
@@ -1,61 +1,61 @@
-//package miu.edu.transactionmanagementsystem.filter;
-//
-//import lombok.SneakyThrows;
-//import miu.edu.transactionmanagementsystem.exception.ExceptionResponse;
-//import miu.edu.transactionmanagementsystem.util.ObjectMapperUtil;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.time.LocalDateTime;
-//
-//@Component
-//public class JwtFilter implements Filter {
-//
-//
-//    @SneakyThrows
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        String url = String.valueOf(httpRequest.getRequestURL());
-//        if (url.contains("/login")) {
-//            chain.doFilter(request, response);
-//        } else {
-////            String header = httpRequest.getHeader("Authorization");
-////            try {
-////                //header
-////                //decrypt the jwt
-////                header.contains("test");
-////                chain.doFilter(request, response);
-////            } catch (Exception e) {
-////                setErrorResponse(HttpStatus.BAD_REQUEST, (HttpServletResponse) response, e);
-////                e.printStackTrace();
-////            }
-//            chain.doFilter(request,response);
-//        }
-//    }
-//
-//    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable ex) {
-//        response.setStatus(status.value());
-//        response.setContentType("application/json");
-//        // A class used for errors
-//        ExceptionResponse apiError = ExceptionResponse
-//                .builder()
-//                .errorMessage("Unauthorized")
-//                .timeStamp(LocalDateTime.now())
-//                .debugMessage("No Header found")
-//                .responseStatus(HttpStatus.UNAUTHORIZED)
-//                .responseCode(401).build();
-//        try {
-//            String json = ObjectMapperUtil.convertToJson(apiError);
-//            System.out.println(json);
-//            response.getWriter().write(json);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//
+package miu.edu.transactionmanagementsystem.filter;
+
+import lombok.SneakyThrows;
+import miu.edu.transactionmanagementsystem.exception.ExceptionResponse;
+import miu.edu.transactionmanagementsystem.util.ObjectMapperUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class JwtFilter implements Filter {
+
+
+    @SneakyThrows
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String url = String.valueOf(httpRequest.getRequestURL());
+        if (url.contains("/login")) {
+            chain.doFilter(request, response);
+        } else {
+//            String header = httpRequest.getHeader("Authorization");
+//            try {
+//                //header
+//                //decrypt the jwt
+//                header.contains("test");
+//                chain.doFilter(request, response);
+//            } catch (Exception e) {
+//                setErrorResponse(HttpStatus.BAD_REQUEST, (HttpServletResponse) response, e);
+//                e.printStackTrace();
+//            }
+            chain.doFilter(request,response);
+        }
+    }
+
+    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable ex) {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+        // A class used for errors
+        ExceptionResponse apiError = ExceptionResponse
+                .builder()
+                .errorMessage("Unauthorized")
+                .timeStamp(LocalDateTime.now())
+                .debugMessage("No Header found")
+                .responseStatus(HttpStatus.UNAUTHORIZED)
+                .responseCode(401).build();
+        try {
+            String json = ObjectMapperUtil.convertToJson(apiError);
+            System.out.println(json);
+            response.getWriter().write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
