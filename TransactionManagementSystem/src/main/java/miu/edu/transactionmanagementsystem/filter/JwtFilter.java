d5876923304bc65ca3de15763d844911150056f1
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-17 23:39:05 -0500

    complete

--- TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/filter/JwtFilter.java
+++ TransactionManagementSystem/src/main/java/miu/edu/transactionmanagementsystem/filter/JwtFilter.java
@@ -24,17 +24,16 @@ public class JwtFilter implements Filter {
         if (url.contains("/login")) {
             chain.doFilter(request, response);
         } else {
-//            String header = httpRequest.getHeader("Authorization");
-//            try {
-//                //header
-//                //decrypt the jwt
-//                header.contains("test");
-//                chain.doFilter(request, response);
-//            } catch (Exception e) {
-//                setErrorResponse(HttpStatus.BAD_REQUEST, (HttpServletResponse) response, e);
-//                e.printStackTrace();
-//            }
-            chain.doFilter(request,response);
+            String header = httpRequest.getHeader("Authorization");
+            try {
+                //header
+                //decrypt the jwt
+                header.contains("test");
+                chain.doFilter(request, response);
+            } catch (Exception e) {
+                setErrorResponse(HttpStatus.BAD_REQUEST, (HttpServletResponse) response, e);
+                e.printStackTrace();
+            }
         }
     }
 
