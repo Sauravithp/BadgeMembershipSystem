3638e1fa835227464f503acbb5156fc0ea4711ac
Abanoub <Abanob.George@vodafone.com>
2022-05-18 11:57:04 -0500

    Remove Entity web service.

--- Entity/src/main/java/miu/edu/entity/BadgeRepository.java
+++
@@ -1,9 +0,0 @@
-package miu.edu.entity;
-
-import miu.edu.entity.model.Badge;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BadgeRepository extends JpaRepository<Badge,Long> {
-}
